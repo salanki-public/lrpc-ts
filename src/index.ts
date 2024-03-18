4c8b32a661b4aa882d272c309e384c093d8509da
Salánki Csaba <salanki.csaba@servantes.hu>
2024-03-18 06:40:10 +0100

    small changes

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@
  * * client sends Requests, ges back a Response
  * * you can use it with express.js, or whatever you want
  * * each request contains one and only one function call (no batch execution)
- * * each function call contains one and zero argument. Argument must be an object, an array or a primitive.
+ * * each function call contains one or zero argument. Argument must be an object, an array or a primitive.
  * 
  * @example
  * ```
